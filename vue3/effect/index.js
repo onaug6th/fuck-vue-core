c7f471021dd248f44150ff7ff6b0a2fb41d9f544
杨文杰 <592986151@qq.com>
2022-08-29 17:52:43 +0800

    feat: 注释补全

--- vue3/effect/index.js
+++ vue3/effect/index.js
@@ -1,22 +1,34 @@
 /**
- * @typedef { Map<string, Set<Function>> } TDepsMap 依赖Map
+ * 副作用函数
+ * @typedef TEffect
+ * @property { Array<TEffects> } deps 一个数组，数组项是某个属性的副作用函数数组
+ * @property { TEffectOptions } options 副作用函数配置
  */
 
 /**
- * @typedef { WeakMap.<object, TDepsMap> } TBucket 存储副作用函数的桶
- */
-
-
-/**
- * @typedef TBtn 按钮类型
- * @property { string } key 按钮键名
- * @property { string } text 按钮文本
- * @property { string } className 按钮样式类名
- * @property { boolean } isLeft 是否优先展示于左侧隐藏菜单
+ * 副作用函数配置
+ * @typedef TEffectOptions
+ * @property { boolean } lazy 是否懒执行
+ * @property { Function } scheduler 调度任务
  */
 
 /**
- * 存储副作用函数的桶
+ * 某个属性的副作用函数数组
+ * @typedef { Set<TEffect> } TEffects
+ */
+
+/**
+ * 对象属性依赖
+ * @typedef { Map<string, TEffects> } TDepsMap
+ */
+
+/**
+ * 桶
+ * @typedef { WeakMap.<Object, TDepsMap> } TBucket
+ */
+
+/**
+ * 桶，存放着每个对象的对象属性依赖
  * @type { TBucket }
  */
 const bucket = new WeakMap()
@@ -65,23 +77,22 @@ function track(target, key) {
         bucket.set(target, depsMap)
     }
 
-    //  属性的更新函数Set
-    let deps = depsMap.get(key)
-    if (!deps) {
-        deps = new Set()
-        depsMap.set(key, deps)
+    let effects = depsMap.get(key)
+    if (!effects) {
+        effects = new Set()
+        depsMap.set(key, effects)
     }
 
-    //  对该对象的属性添加更新函数
-    deps.add(activeEffect)
-    //  对此更新函数添加更新函数Set
-    activeEffect.deps.push(deps)
+    //  该对象的属性的更新队列添加副作用函数
+    effects.add(activeEffect)
+    //  副作用函数的deps添加effects，是为了在每次执行的时候都能清空属性的依赖
+    activeEffect.deps.push(effects)
 }
 
 /**
- * 触发已劫持对象的某个属性拥有的全部更新函数
- * @param {*} target 
- * @param {*} key 
+ * 执行某个属性的全部副作用函数
+ * @param { object } target 目标对象
+ * @param { string } key 对象属性
  */
 function trigger(target, key) {
     //  如该对象没有依赖Map，说明未曾依赖收集过，直接返回
@@ -90,68 +101,79 @@ function trigger(target, key) {
         return
     }
 
-    //  获取该属性的更新函数数组
+    //  获取该属性的副作用函数
     const effects = depsMap.get(key)
 
-    if (effects) {
-        effects.forEach(effectFn => {
-            //  避免反复自身调用
-            if (effectFn === activeEffect) {
-                return
-            }
+    if (!effects) {
+        return
+    }
 
+    /**
+     * 关键操作，必须使用一个新的Set
+     * 因为后续effectFn依赖收集，会对同一个属性进行delete和add。会让原先set的forEach产生无限循环
+     */
+    const newSetArr = new Set(effects)
+
+    newSetArr.forEach(effectFn => {
+        //  避免反复自身调用
+        if (effectFn !== activeEffect) {
             //  存在调度任务
             if (effectFn.options.scheduler) {
                 effectFn.options.scheduler(effectFn)
             }
-            //  否则执行更新函数
+            //  否则执行副作用函数
             else {
                 effectFn()
             }
-        })
-    }
+        }
+    })
 }
 
 /**
- * 当前激活的 effect 函数
- * @type { Function }
+ * 当前激活的副作用函数
+ * @type { TEffect }
  */
 let activeEffect
+
 /**
- * effect 栈
+ * 副作用函数栈
  * 在嵌套effect函数时，为了保证track函数收集的effect为正确的
- * @type { Array<Function> }
+ * @type { Array<TEffect> }
  */
 const effectStack = []
 
 /**
- * @typedef { { lazy: boolean, scheduler: Function } } TEffectOptions 存储副作用函数的桶
+ * 将业务函数注册为副作用函数
+ * @param { Function } fn 业务函数
+ * @param { TEffectOptions } options 副作用函数配置
  */
-
-/**
- * effect函数
- * @param { Function } fn 
- * @param { TEffectOptions } options 
- */
-function effect(fn, options = {}) {
+function effectRegister(fn, options = {}) {
+    /**
+     * 副作用函数
+     * @type { TEffect }
+     * @returns 
+     */
     const effectFn = () => {
         //  清空此副作用函数依赖的对象属性
         cleanup(effectFn)
 
-        //  当调用 effect 注册副作用函数时，将副作用函数赋值给 activeEffect
+        /**
+         * 当注册副作用函数时，将副作用函数赋值给 activeEffect
+         * 为了在执行业务函数
+         */
         activeEffect = effectFn
         // 在调用副作用函数之前将当前副作用函数压栈
         effectStack.push(effectFn)
 
-        //  执行目标函数
+        //  执行业务函数
         const res = fn()
+
         //  在当前副作用函数执行完毕后，将当前副作用函数弹出栈，并还原 activeEffect 为之前的值
         effectStack.pop()
         activeEffect = effectStack[effectStack.length - 1]
 
         return res
     }
-
 
     //  将 options 挂在到 effectFn 上
     effectFn.options = options
@@ -166,18 +188,19 @@ function effect(fn, options = {}) {
 }
 
 /**
- * 将effectFn绑定的依赖属性去掉
- * @param { Function } effectFn 
+ * 将副作用函数绑定的依赖关系去掉
+ * @param { TEffect } effectFn 副作用函数
  */
 function cleanup(effectFn) {
     for (let i = 0; i < effectFn.deps.length; i++) {
         const deps = effectFn.deps[i]
         deps.delete(effectFn)
     }
+
     effectFn.deps.length = 0
 }
 
 //  一个模拟的表达式 {{ name + age }}
-effect(() => {
+effectRegister(() => {
     console.log(proxyData.name + proxyData.age)
 })
