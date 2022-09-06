1fba39508266ed8fb3219106b2d06a4184561082
杨文杰 <592986151@qq.com>
2022-09-06 23:12:33 +0800

    feat: types、lesson1

--- types/index.js
+++ types/index.js
@@ -1,8 +1,24 @@
 /**
  * 副作用函数
  * @typedef TEffect
- * @property { Array<TEffects> } deps 一个数组，数组项是某个属性的副作用函数数组
- * @property { TEffectOptions } options 副作用函数配置
+ * @property { Array<TDeps> } deps
+ * 一个数组，数组项是某个属性的副作用函数数组
+ * 
+ * 在副作用函数执行时，副作用函数内的响应式属性被读取。会触发track依赖收集
+ * 
+ * 进行依赖收集时，会将该属性的副作用函数set存入deps
+ * 
+ * 此值的存在意义是，为了保证响应式属性变化时。每次收集和触发的副作用函数都是最新的，具体工作原理如下：
+ * 
+ * 在副作用函数执行时，都会执行cleanup方法。循环deps数组，将属性的副作用函数set把正在激活的副作用函数删掉。
+ * 
+ * 原因是副作用函数里可能存在分支条件，分支条件不满足时。某些属性的变化我们就不需要关心了，避免执行无用的逻辑。
+ * 
+ * 所以每次副作用函数执行时，都会经历cleanup——依赖收集，这样的过程
+ * 
+ * 保证响应式属性变化时，响应式属性都能收集到最新的依赖
+ * 
+ * @property { TEffectOptions } options 副作用函数的配置
  */
 
 /**
@@ -13,16 +29,26 @@
  */
 
 /**
- * 某个属性的副作用函数数组
- * @typedef { Set<TEffect> } TEffects
+ * 存储副作用函数的set
+ * 
+ * 结构：Set<副作用函数>
+ * @typedef { Set<TEffect> } TDeps
  */
 
 /**
- * 对象属性依赖
- * @typedef { Map<string, TEffects> } TDepsMap
+ * 依赖map
+ * 
+ * 存储每个对象属性的依赖
+ * 
+ * 结构：属性-副作用函数
+ * @typedef { Map<string, TDeps> } TDepsMap
  */
 
 /**
- * 桶
+ * 储存副作用函数的桶
+ * 
+ * 存储响应式对象的依赖map
+ * 
+ * 结构：响应式对象——依赖map
  * @typedef { WeakMap.<Object, TDepsMap> } TBucket
  */
---
+++ vue3/响应式系统/1. 最基本的响应式系统/index.js
@@ -0,0 +1,117 @@
+/**
+ * 储存副作用函数的桶
+ * @type { TBucket }
+ */
+const bucket = new WeakMap()
+
+/**
+ * 实例数据
+ */
+const data = {
+    showNameFirst: true,
+    name: 'august',
+    age: 25
+}
+
+/**
+ * 对象进行响应式处理
+ */
+const _data = new Proxy(data, {
+    get(target, key) {
+        //  该对象的属性被读取时，进行依赖收集
+        track(target, key)
+        //  返回属性值
+        return target[key]
+    },
+    set(target, key, newVal) {
+        //  该对象的属性被修改时，设置属性值
+        target[key] = newVal
+        //  对象属性值发生变化，执行副作用函数
+        trigger(target, key)
+    }
+})
+
+/**
+ * 对已劫持对象的某个属性进行依赖收集
+ * @param { object } target 目标对象
+ * @param { string } key 对象属性
+ */
+function track(target, key) {
+    //  如没有正在运行中的effect，说明并不需要收集
+    if (!activeEffect) {
+        return
+    }
+
+    /**
+     * 依赖Map
+     * @type { TDepsMap }
+     */
+    let depsMap = bucket.get(target)
+    if (!depsMap) {
+        depsMap = new Map()
+        bucket.set(target, depsMap)
+    }
+
+    /**
+     * 副作用函数set
+     * @type { TDeps }
+     */
+    let deps = depsMap.get(key)
+    if (!deps) {
+        deps = new Set()
+        depsMap.set(key, deps)
+    }
+
+    //  属性的更新队列添加副作用函数
+    deps.add(activeEffect)
+}
+
+/**
+ * 执行某个属性的全部副作用函数
+ * @param { object } target 目标对象
+ * @param { string } key 对象属性
+ */
+function trigger(target, key) {
+    //  如该对象没有副作用函数Map，说明未曾依赖收集过，直接返回
+    const depsMap = bucket.get(target)
+    if (!depsMap) {
+        return
+    }
+
+    /**
+     * 副作用函数set
+     * @type { TDeps }
+     */
+    const deps = depsMap.get(key)
+
+    if (!deps) {
+        return
+    }
+
+    deps.forEach(effectFn => {
+        effectFn()
+    })
+}
+
+/**
+ * 当前激活的副作用函数
+ * @type { TEffect }
+ */
+let activeEffect
+
+/**
+ * 将业务函数注册为副作用函数
+ * @param { Function } fn 业务函数
+ */
+function effect(fn) {
+    /**
+     * 当注册副作用函数时，将副作用函数赋值给 activeEffect
+     */
+    activeEffect = fn
+
+    //  执行业务函数
+    fn()
+
+    //  还原activeEffect
+    activeEffect = undefined
+}
